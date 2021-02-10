0b4654ff4486b127389bebadc1ee1845de47b961
Bruno Diaz <bruno.rios@web-experto.com.ar>
2021-02-09 22:46:01 -0300

    fist commit

---
+++ components/Component.js
@@ -0,0 +1,26 @@
+// import { StatusBar } from 'expo-status-bar';
+import React, { Component, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Yugi extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Yugi
